ab87d4598bff37f2706beb199c5575a6259f0880
Eleaus-Hossain-Evan <evanhossain62@gmail.com>
2022-05-21 22:23:27 +0600

    signing report

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,16 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'infastructure/service.dart';
 import 'presentation/home_page.dart';
 
+// > Task :app:signingReport
+// Variant: debug
+// Config: debug
+// Store: C:\Users\evanh\.android\debug.keystore
+// Alias: AndroidDebugKey
+// MD5: 74:53:97:88:37:5D:89:95:BB:77:92:E4:C1:E2:1E:09
+// SHA1: FE:A0:EA:B3:FD:F8:B1:6F:07:0B:22:60:37:93:58:96:05:EB:71:AB
+// SHA-256: 6E:C1:CC:48:6B:96:69:9E:86:88:D5:E2:B3:7D:64:4D:5A:74:0C:C0:A8:7E:F3:8C:6B:7E:BA:48:57:25:45:C0
+// Valid until: Monday, April 29, 2052
+
 Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
   ///Onclick listener
   NotificationService.display(message);
